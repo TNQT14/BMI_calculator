7a79ae5b7a0d1927346032e655edaaed5abb2360
Trần Thị Mỹ Nhung <75101819+mynhungg@users.noreply.github.com>
2023-08-13 16:03:25 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -23,6 +23,7 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     List<String> lang = ['vi', 'VN'];
+    //aaaa
     
     void updateLanguage(List<String> newLang) {
     setState(() {
