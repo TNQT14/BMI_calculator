60983b2e890287ffd70290ea40aa86c989586f3d
Trần Thị Mỹ Nhung <75101819+mynhungg@users.noreply.github.com>
2023-08-13 15:43:40 +0700

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,6 +13,7 @@ import 'package:bmi_calculator/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
+    //test
     await tester.pumpWidget(const MyApp());
 
     // Verify that our counter starts at 0.
